332722656bce9c5c9ee995f5037699c535893373
AlixiaDae <lizziejones1208@gmail.com>
2023-05-27 01:34:18 +0800

    Remove home module
    Combine home module into website module

--- src/website.js
+++ src/website.js
@@ -1,5 +1,9 @@
-import loadHome from "./home"
 import Github from './images/github-mark-white.png'
+import Feast from './images/feast.jpg'
+import Stew from './images/stew.jpg'
+import Yogurt from './images/yogurt.jpg'
+import Porridge from './images/porridge.jpg'
+import Flatbread from './images/flatbread.png'
 
 function createHeader() {
     const header = document.createElement("header")
@@ -18,6 +22,35 @@ function createMain() {
     const main = document.createElement("main")
     main.classList.add("main")
     main.setAttribute("id", "main")
+
+    const feastImage = document.createElement("img")
+    feastImage.classList.add("feast-image")
+    feastImage.src = Feast
+    feastImage.alt = "Feast"
+    main.appendChild(feastImage)
+
+    const recipeWrapper = document.createElement("div")
+    recipeWrapper.classList.add("recipe-wrapper")
+    main.appendChild(recipeWrapper)
+
+    const recipeSubheader = document.createElement("div")
+    recipeSubheader.classList.add("recipe-subheader")
+    recipeSubheader.textContent = "Today's Specials"
+    recipeWrapper.appendChild(recipeSubheader)
+
+    //Recipe Grid
+
+    const recipeGrid = document.createElement("div")
+    recipeGrid.classList.add("recipe-grid")
+    recipeWrapper.appendChild(recipeGrid)
+
+    recipeGrid.appendChild(createRecipeCard("Hearty Viking Stew", Stew))
+
+    recipeGrid.appendChild(createRecipeCard("Viking Flatbread", Flatbread))
+
+    recipeGrid.appendChild(createRecipeCard("Skyr with Honey and Nuts", Yogurt))
+
+    recipeGrid.appendChild(createRecipeCard("Viking Barley Porridge", Porridge))
 
     return main
 }
@@ -43,13 +76,40 @@ function createFooter() {
     return footer
 }
 
+function createRecipeCard(name, imageSrc) {
+    const recipeCard = document.createElement("div")
+    recipeCard.classList.add("recipe-card")
+
+    const recipeImage = document.createElement("img")
+    recipeImage.src = imageSrc
+    recipeImage.alt = name
+    recipeCard.appendChild(recipeImage)
+
+    const recipeName = document.createElement("div")
+    recipeName.textContent = name
+    recipeCard.appendChild(recipeName)
+    
+    return recipeCard
+}
+
+function setActiveCard(card) {
+    const recipeCards = document.querySelectorAll(".recipe-card")
+
+    recipeCards.forEach(recipeCard => {
+        if(recipeCard !== this) {
+            recipeCard.classList.remove("active")
+        }
+    });
+
+    card.classList.add("active")
+}
+
 function initializeWebsite() {
     const content = document.querySelector(".content")
 
     content.appendChild(createHeader())
     content.appendChild(createMain())
     content.appendChild(createFooter())
-    loadHome()
 }
 
 export default initializeWebsite
