4935de4babe74a36bc283061e290415adb7d4867
AlixiaDae <lizziejones1208@gmail.com>
2023-05-28 04:11:30 +0800

    Finish Today Special content

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Font designed by Burhan Afif at <a>https://hanscostudio.com</a>
 
 
 Photo by <a href="https://unsplash.com/@brookelark?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Brooke Lark</a> on <a href="https://unsplash.com/photos/_v3fy536vpk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+Star rating photo by <a href="https://www.freepik.com/free-vector/simple-star-rating_1014846.htm#query=star%20rating&position=6&from_view=keyword&track=ais">Image by starline</a> on Freepik
   
 
 Recipes from <a>https://www.vikingwarriorshirts.com/blogs/news/10-delicious-authentic-viking-recipes</a>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,6 @@
 import Feast from '../images/feast.jpg'
+import Stew from '../images/stew.jpg'
+import Star from '../images/star.jpg'
 
 function createHome() {
     const home = document.createElement("div");
@@ -7,23 +9,71 @@ function createHome() {
     const subHeaderWrapper = document.createElement("div")
     subHeaderWrapper.classList.add("subheader-wrapper")
 
-    const subheaderOneWrapper = document.createElement("div")
-    subheaderOneWrapper.classList.add("subheader-one-wrapper")
+    const mottoWrapper = document.createElement("div")
+    mottoWrapper.classList.add("motto-wrapper")
 
-    const subheaderOne = document.createElement("h2")
-    subheaderOne.textContent = "From Asgard's Finest"
+    const motto = document.createElement("h2")
+    motto.textContent = "From Asgard's Finest"
 
-    const subheaderOneContent = document.createElement("p")
-    subheaderOneContent.classList.add("subheader-one-content")
-    subheaderOneContent.textContent = "Straight from the fields of Freyr, only the freshest and most delicious of ingredients and recipes for the Einherjar"
+    const mottoContent = document.createElement("p")
+    mottoContent.classList.add("motto-content")
+    mottoContent.textContent = "Straight from the fields of Freyr, only the most delicious of recipes for our brave Einherjar"
+
+    const todayWrapper = document.createElement("div")
+    todayWrapper.classList.add("today-wrapper")
+
+    const todaySpecial = document.createElement("h2")
+    todaySpecial.textContent = "Today's Special!!!"
+
+    const todayRecipeWrapper = document.createElement("div")
+    todayRecipeWrapper.classList.add("today-recipe-wrapper")
+
+    todayRecipeWrapper.appendChild(createTodayRecipe("Hearty Viking Stew", Stew, Star, '"A stew fit for a warrior after a long day\'s battle"'))
 
     home.appendChild(subHeaderWrapper)
-    subHeaderWrapper.appendChild(subheaderOneWrapper)
-    subheaderOneWrapper.appendChild(subheaderOne)
-    subheaderOneWrapper.appendChild(subheaderOneContent)
+    home.appendChild(todayWrapper)
+    subHeaderWrapper.appendChild(mottoWrapper)
+    mottoWrapper.appendChild(motto)
+    mottoWrapper.appendChild(mottoContent)
+    todayWrapper.appendChild(todaySpecial)
+    home.appendChild(todayRecipeWrapper)
   
     return home;
   }
+  
+function createTodayRecipe(name, imageSrc, starSrc, description) {
+    const wrapper = document.createElement("div")
+    wrapper.classList.add("special-wrapper")
+    
+    const todayCard = document.createElement("div")
+    todayCard.classList.add("today-card")
+
+    const recipeImage = document.createElement("img")
+    recipeImage.src = imageSrc
+    recipeImage.alt = name
+
+    const recipeName = document.createElement("div")
+    recipeName.textContent = name
+
+    const descriptionWrapper = document.createElement("div")
+    descriptionWrapper.classList.add("description-wrapper")
+
+    const recipeDescription = document.createElement("div")
+    recipeDescription.classList.add("description")
+    recipeDescription.textContent = description
+
+    const starRating = document.createElement("img")
+    starRating.src = starSrc
+    
+    wrapper.appendChild(todayCard)
+    wrapper.appendChild(descriptionWrapper)
+    descriptionWrapper.appendChild(recipeDescription)
+    descriptionWrapper.appendChild(starRating)
+    todayCard.appendChild(recipeImage)
+    todayCard.appendChild(recipeName)
+
+    return wrapper
+}
 
 function loadHome() {
     const main = document.getElementById("main");
